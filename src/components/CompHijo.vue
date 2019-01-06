cca04ad74f5c297bf1e6788ea259b86c7cb0f01f
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2019-01-06 14:53:40 -0500

    All

---
+++ src/components/CompHijo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ lista }}</h2>
+    <input v-model="item" type="string" />
+    <button @click="$emit('list-added', item)">
+     Agregar algo
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompHijo",
+  props: {
+    lista: Array
+  },
+  data() {
+    return {
+      item: ''
+    };
+  }
+};
+</script>
---
+++ src/components/CompPadre.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <CompHijo
+      :lista="lista"
+      @list-added="lista.push($event)"
+    />
+  </div>
+</template>
+
+<script>
+import CompHijo from "./CompHijo";
+
+export default {
+  name: "CompPadre",
+  data() {
+    return {
+      lista: ['Arbol', 'Perro', 'Caballo']
+    };
+  },
+  components: {
+    CompHijo
+  }
+};
+</script>
